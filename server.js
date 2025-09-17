6a779f7f02e6f9f22db5392437f7b8a584e0f4fa
stefanstojanovicstefa-creator <stefanstojanovicstefa@gmail.com>
2025-09-17 17:59:48 +0200

    server.js

--- server.js
+++ server.js
@@ -117,6 +117,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Server pokrenut na portu ${PORT}`);
-
->>>>>>> e846ee62ee3bc210cdc0972dc93d8e36fe24e182
 });
+
